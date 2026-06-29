2321ee6981d10749139a055117d98deb20bb1a63
Bernardo Miguel Pires Kolmer <bpires-k@c1r7s8.42porto.com>
2026-06-29 17:09:01 +0100

    dia 1

---
+++ helper_funct.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helper_funct.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpires-k <bpires-k@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 15:46:59 by bpires-k          #+#    #+#             */
+/*   Updated: 2026/06/29 16:03:55 by bpires-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+float	compute_disorder(int *a, int size)
+{ 
+	int	mistakes;
+	int	i;
+	int	j;
+	int	total_pairs;
+
+	mistakes = 0;
+	i = 0;
+	total_pairs = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size - 1)
+		{
+			total_pairs += 1;
+			if (a[i] > a[j])
+				mistakes += 1;
+		}
+	}
+	return (mistakes / total_pairs);
+}
