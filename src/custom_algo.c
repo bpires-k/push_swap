0a148df1850288db040e7875286e861ee4737aa3
Kolmer no pc do gui <bkolmer@outlook.pt>
2026-07-30 10:26:58 +0100

    adaptive_sort feito

---
+++ src/custom_algo.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   custom_algo.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guilh <emda-sil@student.42school.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/30 10:13:36 by guilh             #+#    #+#             */
+/*   Updated: 2026/07/30 10:26:25 by guilh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+t_dlist	**adaptive_sort(t_dlist **a)
+{
+	float	cd_a;
+
+	cd_a = compute_disorder(a);
+	if (cd_a < 0.2)
+		return (simple_sort(a));
+	else if (cd_a >= 0.2 && cd_a < 0.5)
+		return (medium_sort(a));
+	else
+		return (complex_sort(a));
+}
